0019a8e683f62d3e192885df44b4c16e05e0326c
149311cb <an.nickhcm2000@gmail.com>
2021-04-18 14:35:00 +0700

    merge branch bang

---
+++ backend/models/question.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var questionSchema  = new mongoose.Schema({
+	PhanHoi: String,
+	CauA: String,
+	CauB: String,
+    CauB: String,
+    CauC: String,
+    CauD: String,
+    DapAn: String,
+    Diem: Number,
+});
+
+var Question = mongoose.model('Questions', questionSchema, 'questions');
+
+module.exports = Question;
