b3eaeaa705c9c28c7c3840899f489c11f23230dd
cubo142 <doducminh8694@gmail.com>
2021-04-25 13:37:34 +0700

    update timetable

--- frontend/src/components/AdminExamTimetable.js
+++ frontend/src/components/AdminExamTimetable.js
@@ -1,12 +1,27 @@
 import React from "react";
-import {Table} from "react-bootstrap";
+import TimetableHeader from '../components/TimetableHeader';
+import {Form,Row,Col,Dropdown,DropdownButton,Button,Table} from "react-bootstrap";
 const AdminExamTimetable = () => {
 
 	return (
+		<div className="timetable">
+		<TimetableHeader/>
+
+		
+  				<div style={{display:"flex",float:"right",marginBottom:"1rem"}}>
+	  				<Form.Label style={{marginTop:'0.7rem',marginRight:'0.7rem'}}>Tuần:</Form.Label>
+	  				<DropdownButton id="namhoc" title="25">
+					  <Dropdown.Item href="#/action-1">1</Dropdown.Item>
+					  <Dropdown.Item href="#/action-2">2</Dropdown.Item>
+					  <Dropdown.Item href="#/action-3">3</Dropdown.Item>
+					</DropdownButton>
+	  			</div>
+		
+
 		<Table striped bordered hover>
 		  <thead>
 		    <tr>
-		      <th>#</th>
+		      <th>STT</th>
 		      <th>Thứ 2</th>
 		      <th>Thứ 3</th>
 		      <th>Thứ 4</th>
@@ -17,84 +32,161 @@ const AdminExamTimetable = () => {
 		    </tr>
 		  </thead>
 		  <tbody>
-		    <tr>
-		      {Array.from({ length: 8 }).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		    <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
 		    </tr>
-		     <tr>
-		      {Array.from({ length: 8}).map((_, index) => (
-		        <th key={index}> </th>
-		      ))}
-		    </tr> 
+		     <tr>  
+		       	<td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		        <td> </td>
+		    </tr>
+
 		    
 		  </tbody>
 		</Table>
+		</div>
 		)
 }
 export default AdminExamTimetable;
